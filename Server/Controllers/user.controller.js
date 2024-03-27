313054f9923f752fe22fa45f1da8d73bb118f45d
roshan-acharya <roshanacharya9808@gmail.com>
2024-03-27 22:36:05 +0545

    login api

--- Server/Controllers/user.controller.js
+++ Server/Controllers/user.controller.js
@@ -1,17 +1,46 @@
-const User=require('../Models/user.model')
+const User = require('../Models/user.model')
 
 //user signup
 
-const PostUsers=async(req,res)=>{
-    try{
-        const{username,password}=req.body;
-        const user= await User.create(req.body)
-        res.status(200).json(user)
+const PostUsers = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    if (!username || !password) {
+      return res.status(422).json({ error: "Credentials must be filled" });
+    }
+    else {
+      try {
+        const isUser = await User.findOne({ username: username });
+        if (isUser) {
+          return res.status(206).json({ error: "User already exists" });
+        }
+        // If user doesn't exist, proceed with further logic
+      } catch (error) {
+        console.error("Error checking for user:", error);
+        return res.status(500).json({ error: "Internal Server Error" });
       }
-      catch(e){
-        res.status(500).json({messgae: e.messgae})
-      }
+
+      const user = await User.create(req.body)
+      return res.status(200).json(user)
+    }
+  }
+  catch (e) {
+    res.status(500).json({ messgae: e.messgae })
+  }
 }
-module.exports={
-    PostUsers
+
+//user login
+const CheckUsers=async(req,res)=>{
+  const {username,password}=req.body;
+  isUser=await User.findOne({username:username ,password:password})
+  if(isUser){
+    return res.status(200).json({messgae : "Credentials matched"})
+  }
+  else{
+    return res.status(206).json({messgae:"Invalid credentials"})
+  }
+}
+module.exports = {
+  PostUsers,
+  CheckUsers
 }
--- Server/Routes/User.js
+++ Server/Routes/User.js
@@ -1,7 +1,8 @@
 const express=require('express');
 const router=express.Router();
-const {PostUsers}=require('../Controllers/user.controller')
+const {PostUsers,CheckUsers}=require('../Controllers/user.controller')
 
 router.post("/user/signup", PostUsers)
+router.post("/user/login", CheckUsers)
 
 module.exports=router;
