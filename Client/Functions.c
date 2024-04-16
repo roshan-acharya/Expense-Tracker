9534279c50df8f1665684f8126704be5ea470dc5
roshan-acharya <roshanacharya9808@gmail.com>
2024-04-16 22:07:18 +0545

    update done

--- Client/Functions.c
+++ Client/Functions.c
@@ -1,23 +1,23 @@
 #include "Expense.h"
 #include <stdio.h>
 #include <curl/curl.h>
-#include<string.h>
+#include <string.h>
 #include <cJSON.h>
-#include<stdlib.h>
+#include <stdlib.h>
 #include <unistd.h>
 
 #define ANSI_ORANGE "\x1b[38;5;208m"
-#define ANSI_RED     "\x1b[31m"
-#define ANSI_GREEN   "\x1b[32m"
-#define ANSI_RESET   "\x1b[0m"
+#define ANSI_RED "\x1b[31m"
+#define ANSI_GREEN "\x1b[32m"
+#define ANSI_RESET "\x1b[0m"
 
 #define MAX_ITEMS 20
 
-//function prototype
+// function prototype
 int deleteExpense();
 int updateExpense();
 
-int struct_n,count=1;
+int struct_n, count = 1;
 struct Expense
 {
     char id[200];
@@ -28,18 +28,14 @@ struct Expense
     char dat[60];
 } send_data;
 
-struct Transaction{
+struct Transaction
+{
     char id[200];
     char desc[50];
     char cat[40];
     char wallet[25];
     int amount;
-}transactions[MAX_ITEMS];
-
-
-
-
-
+} transactions[MAX_ITEMS];
 
 // get data function
 int getData()
@@ -80,22 +76,23 @@ int addExpense(char *id)
 
         // Format the data into JSON
         char data[712]; // Adjust buffer size as needed
-        
+
         snprintf(data, sizeof(data), "{\"id\":\"%s\",\"desc\":\"%s\",\"cat\":\"%s\",\"amount\":%d,\"wallet\":\"%s\"}",
                  id, send_data.description, send_data.cat, send_data.amt, send_data.wallet);
         // Set POST data
-        printf("%s",data);
-       curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
+        printf("%s", data);
+        curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
 
-        //Perform the POST request
-      res = curl_easy_perform(curl);
+        // Perform the POST request
+        res = curl_easy_perform(curl);
         curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status_code);
         // Check for errors
         if (res != CURLE_OK)
         {
             fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         }
-        if(status_code==200){
+        if (status_code == 200)
+        {
             printf("Expense Added");
         }
 
@@ -106,26 +103,28 @@ int addExpense(char *id)
     return 0;
 }
 
-
-//Get expense data
-size_t write_callback_view(void *ptr, size_t size, size_t nmemb, FILE *stream) {
+// Get expense data
+size_t write_callback_view(void *ptr, size_t size, size_t nmemb, FILE *stream)
+{
     return fwrite(ptr, size, nmemb, stream);
 }
-int viewExpense(char *id){
- CURL *curl = curl_easy_init();
-    if (!curl) {
+int viewExpense(char *id)
+{
+    CURL *curl = curl_easy_init();
+    if (!curl)
+    {
         fprintf(stderr, "Error initializing curl.\n");
         return 1;
     }
-   char url[100];
-   strcpy(url, "http://localhost:3000/expense/get/");
-   strcat(url, id);
-    //url for get request
+    char url[100];
+    strcpy(url, "http://localhost:3000/expense/get/");
+    strcat(url, id);
+    // url for get request
     curl_easy_setopt(curl, CURLOPT_URL, url);
 
-    
     FILE *file = fopen("data.json", "wb");
-    if (!file) {
+    if (!file)
+    {
         fprintf(stderr, "Error opening file.\n");
         return 1;
     }
@@ -134,7 +133,8 @@ int viewExpense(char *id){
 
     // Perform the request
     CURLcode res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
+    if (res != CURLE_OK)
+    {
         fprintf(stderr, "Error performing request: %s\n", curl_easy_strerror(res));
         return 1;
     }
@@ -144,10 +144,12 @@ int viewExpense(char *id){
     fclose(file);
     fileRead();
 }
-int fileRead(){
-    count=1;
-     FILE *file = fopen("data.json", "r");
-    if (!file) {
+int fileRead()
+{
+    count = 1;
+    FILE *file = fopen("data.json", "r");
+    if (!file)
+    {
         printf("Error opening file.\n");
         return 1;
     }
@@ -159,11 +161,12 @@ int fileRead(){
     char *json_data = (char *)malloc(fileSize + 1);
     fread(json_data, 1, fileSize, file);
     fclose(file);
-    json_data[fileSize] = '\0'; //terminate the string
+    json_data[fileSize] = '\0'; // terminate the string
 
     // Parse JSON string
     cJSON *root = cJSON_Parse(json_data);
-    if (!root) {
+    if (!root)
+    {
         printf("Error parsing JSON.\n");
         free(json_data);
         return 1;
@@ -171,26 +174,28 @@ int fileRead(){
     free(json_data);
 
     // Check if root is an array
-    if (!cJSON_IsArray(root)) {
+    if (!cJSON_IsArray(root))
+    {
         printf("JSON root is not an array.\n");
         cJSON_Delete(root);
         return 1;
     }
 
-    struct_n= cJSON_GetArraySize(root);
-    
+    struct_n = cJSON_GetArraySize(root);
 
     // Getting json
-    for (int i = 0; i < struct_n && i < MAX_ITEMS; i++) {
+    for (int i = 0; i < struct_n && i < MAX_ITEMS; i++)
+    {
         cJSON *transaction = cJSON_GetArrayItem(root, i);
         cJSON *id = cJSON_GetObjectItem(transaction, "_id");
         cJSON *desc = cJSON_GetObjectItem(transaction, "desc");
         cJSON *cat = cJSON_GetObjectItem(transaction, "cat");
         cJSON *wallet = cJSON_GetObjectItem(transaction, "wallet");
         cJSON *amount = cJSON_GetObjectItem(transaction, "amount");
-//copy each json
+        // copy each json
 
-        if (id && desc && cat && amount) {
+        if (id && desc && cat && amount)
+        {
             strcpy(transactions[i].id, id->valuestring);
             strcpy(transactions[i].desc, desc->valuestring);
             strcpy(transactions[i].cat, cat->valuestring);
@@ -199,14 +204,13 @@ int fileRead(){
                 strcpy(transactions[i].wallet, wallet->valuestring);
             else
                 transactions[i].wallet[0] = '\0';
-
-          
         }
     }
     printf("\033[H\033[J");
-    printf(ANSI_ORANGE"\033[1mYour Expenses on recent times : \n\n"ANSI_RESET);
-    printf(ANSI_GREEN"\033[1m%-10s %-25s %-10s %-20s %-10s\n", "S.N.", "Description", "Category", "Wallet", "Amount"ANSI_RESET);
-    for (int i = 0; i < struct_n && i < MAX_ITEMS; i++) {
+    printf(ANSI_ORANGE "\033[1mYour Expenses on recent times : \n\n" ANSI_RESET);
+    printf(ANSI_GREEN "\033[1m%-10s %-25s %-10s %-20s %-10s\n", "S.N.", "Description", "Category", "Wallet", "Amount" ANSI_RESET);
+    for (int i = 0; i < struct_n && i < MAX_ITEMS; i++)
+    {
         printf("%-10d %-25s %-10s %-20s %-10d\n", count, transactions[i].desc, transactions[i].cat, transactions[i].wallet, transactions[i].amount);
         count++;
     }
@@ -215,96 +219,108 @@ int fileRead(){
     cJSON_Delete(root);
 
     return 0;
-
 }
-int deleteExpense(){
+int deleteExpense()
+{
     int sn;
     printf("Enter S.N for the expense to delete : ");
-    scanf("%d",&sn);
-    if(sn>count){
+    scanf("%d", &sn);
+    if (sn > count)
+    {
         printf("Invalid Selection");
     }
-    //handling delete request
+    // handling delete request
     CURL *curl;
     CURLcode res;
     curl = curl_easy_init();
 
-    //url for delete request
+    // url for delete request
     long status_code;
     char url[100];
     strcpy(url, "http://localhost:3000/expense/del/");
-      curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_URL, url);
     curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "DELETE");
     curl_easy_perform(curl);
     curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &status_code);
-    
-    if(status_code==200){
-        printf(ANSI_RED"\n Deleted Successfull"ANSI_RESET);
+
+    if (status_code == 200)
+    {
+        printf(ANSI_RED "\n Deleted Successfull" ANSI_RESET);
     }
 
     // Cleanup libcurl
     curl_easy_cleanup(curl);
     sleep(3);
-    printf("\033[H\033[J"); 
+    printf("\033[H\033[J");
     return 0;
 }
-int updateExpense(){
+int updateExpense()
+{
     int sn;
     char data[100];
     printf("Enter S.N for the expense to delete : ");
-    scanf("%d",&sn);
+    scanf("%d", &sn);
 
-printf("\n Enter Category (%s) : ",transactions[sn-1].cat);
-      scanf("%s",data);;
-      if (strcmp(data, "\n") != 0) {
-        strcpy(transactions[sn-1].cat,data);
-      }
+    printf("\n Enter Category (%s) : ", transactions[sn - 1].cat);
+    scanf("%s", data);
+    ;
+    if (strcmp(data, "\n") != 0)
+    {
+        strcpy(transactions[sn - 1].cat, data);
+    }
 
-    printf("\n Enter Description (%s) : ",transactions[sn-1].desc);
-    scanf("%s",data);
-      if (strcmp(data, "\n") != 0) {
-        strcpy(transactions[sn-1].desc,data);
-      }
-    printf("\n Enter Wallet used (%s) : ",transactions[sn-1].wallet);
-    scanf("%s",data);
-      if (strcmp(data, "\n") != 0) {
-        strcpy(transactions[sn-1].wallet,data);
-      }
+    printf("\n Enter Description (%s) : ", transactions[sn - 1].desc);
+    scanf("%s", data);
+    if (strcmp(data, "\n") != 0)
+    {
+        strcpy(transactions[sn - 1].desc, data);
+    }
+    printf("\n Enter Wallet used (%s) : ", transactions[sn - 1].wallet);
+    scanf("%s", data);
+    if (strcmp(data, "\n") != 0)
+    {
+        strcpy(transactions[sn - 1].wallet, data);
+    }
     int input;
     char newline;
-    printf("\n Enter Amount expensed (%d) : ",transactions[sn-1].amount);
-    if (scanf("%d%c", &input, &newline) == 2 && newline == '\n') {
-        transactions[sn-1].amount = input;
-    } 
+    printf("\n Enter Amount expensed (%d) : ", transactions[sn - 1].amount);
+    if (scanf("%d%c", &input, &newline) == 2 && newline == '\n')
+    {
+        transactions[sn - 1].amount = input;
+    }
 
-      printf("desc : %s \n cat %s \n amount %d \n wallet %s\n",transactions[sn-1].desc,transactions[sn-1].cat,transactions[sn-1].amount,transactions[sn-1].wallet);
-      int status_code;
+    printf("desc : %s \n cat %s \n amount %d \n wallet %s\n", transactions[sn - 1].desc, transactions[sn - 1].cat, transactions[sn - 1].amount, transactions[sn - 1].wallet);
+    int status_code;
     CURL *curl;
     CURLcode res;
-    
-
     // Initialize libcurl
     curl = curl_easy_init();
-    if (curl)
+    if (!curl)
     {
-        // Set URL
-        char url[100];
-       strcpy(url, "http://localhost:3000/expense/update/");
-       strcat(url, transactions[sn-1].id);
-        curl_easy_setopt(curl, CURLOPT_URL, url);
-        struct curl_slist *headers = NULL;
-        headers = curl_slist_append(headers, "Content-Type: application/json");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+        fprintf(stderr, "Failed to initialize libcurl\n");
+        return 1;
+    }
+    // Set URL
+    char url[100];
+    strcpy(url, "http://localhost:3000/expense/update/");
+    strcat(url, transactions[sn - 1].id);
+    curl_easy_setopt(curl, CURLOPT_URL, url);
+    curl_easy_setopt(curl, CURLOPT_CUSTOMREQUEST, "PATCH");
+    // set header type
+    struct curl_slist *headers = NULL;
+    headers = curl_slist_append(headers, "Content-Type: application/json");
+    curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
 
-        // Format the data into JSON
-        char data[712]; // Adjust buffer size as needed
-        printf("%s",url);
-        snprintf(data, sizeof(data), "{\"desc\":\"%s\",\"cat\":\"%s\",\"amount\":%d,\"wallet\":\"%s\"}",
-                 transactions[sn-1].desc, transactions[sn-1].cat, transactions[sn-1].amount, transactions[sn-1].wallet);
-     printf("%s",data);
-      curl_easy_setopt(curl, CURLOPT_POSTFIELDS, data);
-        res = curl_easy_perform(curl);
-    if (res != CURLE_OK) {
+    // Format the data into JSON
+    char datas[712]; // Adjust buffer size as needed
+    printf("%s", url);
+    snprintf(datas, sizeof(datas), "{\"desc\":\"%s\",\"cat\":\"%s\",\"amount\":%d,\"wallet\":\"%s\"}",
+             transactions[sn - 1].desc, transactions[sn - 1].cat, transactions[sn - 1].amount, transactions[sn - 1].wallet);
+
+    curl_easy_setopt(curl, CURLOPT_POSTFIELDS, datas);
+    res = curl_easy_perform(curl);
+    if (res != CURLE_OK)
+    {
         fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
         curl_easy_cleanup(curl);
         return 1;
@@ -312,7 +328,6 @@ printf("\n Enter Category (%s) : ",transactions[sn-1].cat);
 
     // Cleanup libcurl
     curl_easy_cleanup(curl);
-
-      return 0;
-    }
+    curl_slist_free_all(headers);
+    return 0;
 }
